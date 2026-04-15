a61d7893d20027d44337fb702da4c4456c5706d9
veerak0811 <veerak0811@gmail.com>
2026-04-15 15:11:35 +0530

    Initial commit of all files

---
+++ python_basics_veera.py
@@ -0,0 +1,17 @@
+# Task 1: Variables and Data Types
+name = "veerababu"
+age = 38
+height = 1.75
+is_student = True
+
+# Print all values in a single print statement
+print(name, age, height, is_student)
+
+# Task 2: String Formatting
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+# Task 3: Arithmetic Operations
+print("Age in months:", age * 12)
+print("Age in days:", age * 365)
+print("Remainder when age is divided by 7:", age % 7)
+print("Age squared:", age ** 2)
